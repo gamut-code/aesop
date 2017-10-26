4cf924e5b50fe5e501dcdbf416830136522c8ec0
Daniel Mackey <daniel.mackey@gamut.com>
2017-10-26 15:13:15 -0500

    cli will make a temp file based on args

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,19 @@
 #! /usr/bin/env node
+const fs = require('fs');
+const { argv } = process;
 
-console.log('Could you, would you, with a bin');
+const componentName = argv.slice(2)[0];
+
+console.log('Check to see if component already exists...');
+if (!fs.existsSync('tmp')) fs.mkdirSync('tmp');
+if (!fs.existsSync('tmp/dr-seuss')) fs.mkdirSync('tmp/dr-seuss');
+
+if (!fs.existsSync(`tmp/dr-seuss/${componentName}`)) {
+  console.log('Generating component...');
+  fs.writeFileSync(`tmp/dr-seuss/${componentName}`, componentName);
+} else {
+  console.log('Component already exists, continue on...');
+}
+
+console.log('All set!');
+
